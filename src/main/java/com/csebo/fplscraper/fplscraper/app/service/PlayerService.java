a70bf85fd942663530d2be44ff81592da73347eb
Csaba <csaba.arato@finshape.com>
2024-08-21 05:43:15 +0200

    Create GET request to return participant IDs and names of the league

--- src/main/java/com/csebo/fplscraper/fplscraper/app/service/PlayerService.java
+++ src/main/java/com/csebo/fplscraper/fplscraper/app/service/PlayerService.java
@@ -2,8 +2,15 @@ package com.csebo.fplscraper.fplscraper.app.service;
 
 import com.csebo.fplscraper.fplscraper.app.mapper.PlayerMapper;
 import com.csebo.fplscraper.fplscraper.app.repository.PlayerRepository;
-import com.csebo.fplscraper.fplscraper.app.scraper.PlayerScraper;
+import com.csebo.fplscraper.fplscraper.app.scraper.DataScraper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class PlayerService {
@@ -15,7 +22,7 @@ public class PlayerService {
     }
 
     public void savePlayersFromFplServer() {
-        String jsonResponse = PlayerScraper.scrapePlayersFromFplServer();
+        String jsonResponse = DataScraper.executeGetRequest("https://fantasy.premierleague.com/api/bootstrap-static/");
         playerRepository.saveAll(PlayerMapper.mapJsonToPlayerEntity(jsonResponse));
     }
 }
