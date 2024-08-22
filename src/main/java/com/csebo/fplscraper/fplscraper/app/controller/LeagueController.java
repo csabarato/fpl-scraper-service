65e9b32e418e9839d1cb7fbeee4951b71fc21457
Csaba <csaba.arato@gmail.com>
2024-08-22 17:49:47 +0200

    Add JSON utility functions and refine API endpoints
    Introduced JsonConverterUtils for JSON operations and refactored LeagueService to use new utilities. Updated API definitions to replace squads with picks and adjusted LeagueController accordingly. Added tests for JsonConverterUtils and moved DataScraper to HttpRequestUtils for better naming and usage.

--- src/main/java/com/csebo/fplscraper/fplscraper/app/controller/LeagueController.java
+++ src/main/java/com/csebo/fplscraper/fplscraper/app/controller/LeagueController.java
@@ -2,7 +2,8 @@ package com.csebo.fplscraper.fplscraper.app.controller;
 
 import com.csebo.fplscraper.fplscraper.app.service.LeagueService;
 import org.SwaggerCodeGenExample.api.LeagueApi;
-import org.SwaggerCodeGenExample.model.SquadsResponseBody;
+import org.SwaggerCodeGenExample.model.PicksRequestBody;
+import org.SwaggerCodeGenExample.model.PicksResponseBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
@@ -24,7 +25,9 @@ public class LeagueController implements LeagueApi {
     }
 
     @Override
-    public ResponseEntity<SquadsResponseBody> getSquads(Integer leagueId) {
-        return null;
+    public ResponseEntity<PicksResponseBody> getPicks(PicksRequestBody picksRequestBody) {
+        return ResponseEntity.ok()
+                .body(new PicksResponseBody()
+                .picks(leagueService.scrapePicksFromFplServer(picksRequestBody.getPlayerIds(), picksRequestBody.getGameweek())));
     }
 }
---
+++ src/main/java/com/csebo/fplscraper/fplscraper/app/utils/JsonConverterUtils.java
@@ -0,0 +1,63 @@
+package com.csebo.fplscraper.fplscraper.app.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.SwaggerCodeGenExample.model.PlayerPickModel;
+import org.apache.commons.lang3.NotImplementedException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JsonConverterUtils {
+
+    private JsonConverterUtils() {
+        throw new NotImplementedException("Util class, should not be instantiated");
+    }
+
+    public static Map<String, String> extractParticipantsFromJson(String json) {
+
+        Map<String, String> participants = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode standings = objectMapper.readTree(json);
+            for (JsonNode result : standings.get("standings").get("results")){
+                participants.put(result.get("entry").asText(), result.get("player_name").asText());
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to extract participants from json: " + json, e);
+        }
+        return participants;
+    }
+
+    public static void collectPicksFromJson(String json, Map<Integer, List<PlayerPickModel>> picksMap, Integer participantId) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode picks = objectMapper.readTree(json).get("picks");
+            if (picks != null) {
+                for (JsonNode pick : picks) {
+
+                    int playerId = pick.get("element").asInt();
+                    picksMap.computeIfPresent(playerId, (k, v) -> {
+                        PlayerPickModel playerPick = new PlayerPickModel();
+                        playerPick.setId(participantId);
+                        playerPick.setMultiplier(pick.get("multiplier").asInt());
+                        v.add(playerPick);
+                        return v;
+                    });
+
+                    picksMap.computeIfAbsent(playerId, k -> {
+                        PlayerPickModel playerPick = new PlayerPickModel();
+                        List<PlayerPickModel> playerPicks = new ArrayList<>();
+                        playerPick.id(participantId);
+                        playerPick.setMultiplier(pick.get("multiplier").asInt());
+                        playerPicks.add(playerPick);
+                        return playerPicks;
+                    });
+                }
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to extract picks from json: " + json, e);
+        }
+    }
+}
